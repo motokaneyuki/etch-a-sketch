d6ea2cb993066a7dcb24ba35b16abae807e0121a
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-01-31 20:36:55 +0900

    Make grid sizes based on width of container

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('.container');
 
-let size = 16 * 16;
+let size = 16;
+let gridSize = 400 / size; 
 
-for (i = 0; i < size; i++){
+for (i = 0; i < size * size; i++){
     const grid = document.createElement('div'); 
     grid.classList.add('grid');
+    grid.style.width = `${gridSize}px`;
     container.appendChild(grid);
+
     grid.addEventListener('mouseover', () => {
         grid.classList.add('mouseover');
     })
