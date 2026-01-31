dfc765babf427545795aa0ca205f1be50ef89b55
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-01-31 21:13:00 +0900

    Make button create a new grid with users prompt

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const container = document.querySelector('.container');
+const newGridButton = document.querySelector('.newGrid');
 
 let size = 16;
 let gridSize = 400 / size; 
 
-for (i = 0; i < size * size; i++){
-    const grid = document.createElement('div'); 
-    grid.classList.add('grid');
-    grid.style.width = `${gridSize}px`;
-    container.appendChild(grid);
+etchASketch(size);
 
-    grid.addEventListener('mouseover', () => {
-        grid.classList.add('mouseover');
-    })
+function etchASketch(size) {
+    for (let i = 0; i < size * size; i++){
+        const grid = document.createElement('div'); 
+        grid.classList.add('grid');
+        grid.style.width = `${gridSize}px`;
+        container.appendChild(grid);
+
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('mouseover');
+        })
+    }
 }
 
-
+newGridButton.addEventListener('click', () => {
+    container.textContent = '';
+    let result = Number(prompt('What grid size would you like?', 'Choose from 1 - 100'));
+    size = result;
+    gridSize = 400 / result;
+    
+    etchASketch(size);
+})
